53f0079f0c0b64d4c1a8b202c956cdd427e7e96c
Alexey <notlexa>
2022-12-29 19:24:49 +0300

    Alpha Build 0.3.0 [#43] A. CASUAL_PLAYGROUND.js:     1) прокрутка инструментов хотбара на колесо без Ctrl     2) zoom на Ctrl+колесо

--- CASUAL_PLAYGROUND.js
+++ CASUAL_PLAYGROUND.js
@@ -664,19 +664,12 @@ const EntFieldBoard = new engine.Entity({
             switch (mb)
             {
                 case engine.WHEELUP:
-                    if (!globalkeys.Ctrl)
-                    {
-                        if (globalkeys.Shift) current_instrument.scale++;
-                        else this.board_zoom_in(target, 1);
-                    }
+                    if (globalkeys.Shift) current_instrument.scale++;
+                    else if (globalkeys.Ctrl) this.board_zoom_in(target, 1);
                     break;
                 case engine.WHEELDOWN:
-                    if (!globalkeys.Ctrl)
-                    {
-                        if (globalkeys.Shift)
-                            current_instrument.scale = Math.max(current_instrument.scale-1, 1);
-                        else this.board_zoom_out(target, 1);
-                    }
+                    if (globalkeys.Shift) current_instrument.scale = Math.max(current_instrument.scale-1, 1);
+                    else if (globalkeys.Ctrl) this.board_zoom_out(target, 1);
                     break;
             }
         }
@@ -1042,7 +1035,7 @@ const EntFieldSUI = new engine.Entity({
                 }
             }
         }
-        if (globalkeys.Ctrl)
+        if (!(globalkeys.Ctrl || globalkeys.Shift || globalkeys.Alt))
         {
             switch (buttonid)
             {
